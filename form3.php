62220342c2ee0e62d4858763fbb36b1376307ed3
Hoi15A <austin.aa@me.com>
2016-07-12 21:03:36 +0200

    Form 2 Done

--- form3.php
+++ form3.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Wettbewerb - P2</title>
+    <title>Wettbewerb - P3</title>
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="style.css" type="text/css">
   </head>
@@ -15,22 +15,22 @@
     <h1>40 Jahre DoppelKlang - Wettbewerb</h1>
       <div class="form-box">
         <p>
-          Hallo <?php echo $_SESSION['vname']; ?>, bitte beantworte die 1. Frage:<br>
-          Welches Instrument spielt unser Gründungsmitglied Gusti?
+          Hallo <?php echo $_SESSION['vname']; ?>, nun die 2. Frage:<br>
+          Wieviele Bandmitglieder spielen bei uns mit?
         </p>
         <form method="post">
           <table>
             <tr>
-              <td>Triangel </td>
-              <td><input type="radio" name="instrument" value="1" required><br></td>
+              <td>21 </td>
+              <td><input type="radio" name="members" value="1" required><br></td>
             </tr>
             <tr>
-              <td>Dudelsack </td>
-              <td><input type="radio" name="instrument" value="2" required><br></td>
+              <td>69 </td>
+              <td><input type="radio" name="members" value="2" required><br></td>
             </tr>
             <tr>
-              <td>Airhorn </td>
-              <td><input type="radio" name="instrument" value="3" required><br></td>
+              <td>666 </td>
+              <td><input type="radio" name="members" value="3" required><br></td>
             </tr>
             <tr>
               <td><input class="button" type="submit" name="continue" value="Weiter"></td>
@@ -41,8 +41,9 @@
       </div>
 
       <?php
+        echo $_SESSION['instrument'];
         if (isset($_POST['continue'])) {
-          $_SESSION['instrument'] = $_POST['instrument'];
+          $_SESSION['members'] = $_POST['members'];
           header("Location:form3.php");
           }
        ?>
