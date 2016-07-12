afc8309d6ad061a95765e617439ea7d0a754ebfc
Hoi15A <austin.aa@me.com>
2016-07-12 23:41:35 +0200

    IT FUCKING SAVES STUFF NOW

--- save.php
+++ save.php
@@ -26,10 +26,28 @@
         if ($conn->connect_error) {
           die("Connection failed: " . $conn->connect_error);
         }
-        echo "Connected successfully";
+        echo "Connected successfully<br>";
 
-        $sql = "INSERT INTO wettbewerb (vname, name, mail, instrument, members, check1, check2, check3, check4, ageGuess)
-        VALUES ($_SESSION['vname'], $_SESSION['name'], $_SESSION['mail'], $_SESSION['instrument'], $_SESSION['members'], $_SESSION['check1'], $_SESSION['check2'], $_SESSION['check3'], $_SESSION['check4'], $_SESSION['ageGuess'])";
+        $vname = $_SESSION['vname'];
+        $name = $_SESSION['name'];
+        $mail = $_SESSION['mail'];
+        $instrument = $_SESSION['instrument'];
+        $members = $_SESSION['members'];
+        $check1 = $_SESSION['check1'];
+        $check2 = $_SESSION['check2'];
+        $check3 = $_SESSION['check3'];
+        $check4 = $_SESSION['check4'];
+        $ageGuess = $_SESSION['ageGuess'];
+
+        $sql = "INSERT INTO wettbewerb (vname, name, mail, instrument, members, check1, check2, check3, check4, ageGuess) VALUES ('$vname', '$name', '$mail', '$instrument', '$members', '$check1', '$check2', '$check3', '$check4', '$ageGuess')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+
          ?>
       </div>
 
